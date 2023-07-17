2039789a3a066b559a8833531b999c2e5fe71a94
BrunaAquino22 <brunahasting99@gmail.com>
2023-07-17 16:56:57 -0300

    .

---
+++ main.js
@@ -0,0 +1,28 @@
+// mapeamento da rota
+const routes = {
+  "/": "/pages/home.html",
+  "/about": "pages/about.html",
+  "/contact": "/pages/contact.html",
+  404: "/pages/404.html",
+};
+// para não carregar a página - lugar nenhum
+function route(event) {
+  event = event || window.event;
+  event.preventDefault();
+
+  window.history.pushState({}, "", event.target.href);
+  handle();
+}
+
+//manipulação
+
+function handle() {
+  const pathname = window.location.pathname;
+  const route = routes[pathname] || routes[404];
+
+  fetch(route)
+    .then(data => data.text())
+    .then(html => console.log(html));
+
+  console.log(route);
+}
